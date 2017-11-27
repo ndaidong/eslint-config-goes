e36eb49f8e859dc69b4aeab1b5f39ef97883eb54
Dong Nguyen <ndaidong@gmail.com>
2017-11-27 16:56:39 +0700

    v0.0.1 - Initial

---
+++ test/specs/main.js
@@ -0,0 +1,39 @@
+/**
+ * Testing
+ * @ndaidong
+ */
+
+const test = require('tape');
+const {
+  isObject,
+  hasProperty,
+} = require('bellajs');
+
+const config = require('../../index');
+
+const structure = [
+  'parserOptions',
+  'env',
+  'globals',
+  'plugins',
+  'extends',
+  'rules',
+];
+
+test('Testing Shareable Config  object:', (assert) => {
+  assert.ok(isObject(config), 'Shareable Config must be an object.');
+  structure.every((k) => {
+    assert.ok(hasProperty(config, k), `Shareable Config must have the property "${k}"`);
+    return k;
+  });
+
+  assert.ok(
+    config.extends.includes('eslint:recommended'),
+    `config.extends value must include Eslint recommended rules`
+  );
+  assert.ok(
+    config.extends.includes('google'),
+    `config.extends value must include Google style`
+  );
+  assert.end();
+});
